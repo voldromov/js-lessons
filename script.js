f6462489c21ce347c5f59bd658e4b3bc16641140
Dromov <example@gmail.com>
2020-12-04 19:26:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// "use strict;"
+
+// // // // // let number = 5;
+// // // // // const leftBorderWith = 1;
+
+// // // // // number = 10;
+// // // // // console.log(number)
+
+// // // // const obj = {
+// // // //     name: 'John',
+// // // //     age: 25,
+// // // //     isMarried: false
+// // // // };
+
+// // // // console.log(obj."name")
+ 
+// // // alert("Hello")
+
+// // const result = confirm('Are you here?')
+// // console.log(result) 
+
+// const answer = prompt("Вам є 18?", "18");
+// console.log(typeof(answer));
+
+const answers = []
+// answers[0] = prompt('Як вас звати', '')
+// answers[1] = prompt('Як вас фімілія', '')
+// answers[2] = prompt('Скільки вам років', '') 
+ 
+console.log(typeof(null))
+
+
+
